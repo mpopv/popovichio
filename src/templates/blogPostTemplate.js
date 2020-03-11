87ba311b0b8aab20554e33445b1195de7bb4ca49
Matt Popovich <mattpopovich@outlook.com>
2020-03-10 20:06:18 -0700

    post formatting and imported twitter bot Medium post as markdown

--- src/templates/blogPostTemplate.js
+++ src/templates/blogPostTemplate.js
@@ -1,15 +1,57 @@
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import React from 'react';
-import { Layout } from '../components/Layout';
+import styled from 'styled-components';
+import Layout from '../components/Layout';
 
-export default ({ data }) => {
+const Content = styled.div`
+  width: 100%;
+  max-width: 680px;
+  margin: 0 auto;
+  & img {
+    width: 100%;
+  }
+`;
+
+const LinkArea = styled.p`
+  display: inline-flex;
+  align-items: center;
+`;
+
+export default ({ data, pageContext }) => {
   const { frontmatter, body } = data.mdx;
+  const { previous, next } = pageContext;
   return (
     <Layout>
-      <h1>{frontmatter.title}</h1>
-      <p>{frontmatter.date}</p>
-      <MDXRenderer>{body}</MDXRenderer>
+      <Content>
+        <h1>{frontmatter.title}</h1>
+        <p>{frontmatter.date}</p>
+        <MDXRenderer>{body}</MDXRenderer>
+        {previous === false ? null : (
+          <>
+            {previous && (
+              <LinkArea>
+                Previous post:&nbsp;
+                <Link to={previous.fields.slug}>
+                  <p>{previous.frontmatter.title}</p>
+                </Link>
+              </LinkArea>
+            )}
+          </>
+        )}
+        {next === false ? null : (
+          <>
+            {next && (
+              <LinkArea>
+                Next post:&nbsp;
+                <Link to={next.fields.slug}>
+                  <p>{next.frontmatter.title}</p>
+                </Link>
+              </LinkArea>
+            )}
+          </>
+        )}
+      </Content>
     </Layout>
   );
 };
